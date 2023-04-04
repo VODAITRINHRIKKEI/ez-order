b179266c63b2c630f7bcd4a626f824cf7a3b3292
VODAITRINHRIKKEI <116076968+VODAITRINHRIKKEI@users.noreply.github.com>
2023-04-04 09:52:38 +0900

    update

--- src/main/Login.js
+++ src/main/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authentication } from "../app/firebase";
 import { db } from "../app/firebase";
 import { firebase } from "../app/firebase";
@@ -8,7 +8,14 @@ import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import GoogleIcon from "@mui/icons-material/Google";
-export default function Login() {
+import { setToken } from "../slice/loginSlice";
+import { useDispatch } from "react-redux";
+import Home from "./Home";
+function LoadingComponent() {
+  return <div className="loading"></div>;
+}
+
+function LoginComponent() {
   const setUser = (user) => {
     db.collection("user")
       .doc(user.uid)
@@ -43,41 +50,87 @@ export default function Login() {
       });
   };
   return (
-    <div className="login">
-      <div className="loginBox">
-        <p className="LoginBoxTitle">Login</p>
-        <Stack spacing={2}>
-          <TextField
-            id="outlined-basic"
-            label="Username"
-            variant="outlined"
-            fullWidth
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            fullWidth
-          />
-          <Button variant="contained" fullWidth size="large">
-            Login
-          </Button>
-          <Button
-            variant="outlined"
-            fullWidth
-            size="large"
-            onClick={() => {
-              loginWithGoogle();
-            }}
-          >
-            <div className="loginWithGmailButton">
-              <GoogleIcon></GoogleIcon>
-              <span className="loginWithGmailText">Login with Gmail</span>
+    <>
+      <p className="LoginBoxTitle">Login</p>
+      <Stack spacing={2}>
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          fullWidth
+        />
+        <Button variant="contained" fullWidth size="large">
+          Login
+        </Button>
+        <Button
+          variant="outlined"
+          fullWidth
+          size="large"
+          onClick={() => {
+            loginWithGoogle();
+          }}
+        >
+          <div className="loginWithGmailButton">
+            <GoogleIcon></GoogleIcon>
+            <span className="loginWithGmailText">Login with Gmail</span>
+          </div>
+        </Button>
+      </Stack>
+    </>
+  );
+}
+
+
+export default function Login() {
+  const [loading, isLoading] = useState(false);
+  const [needLogging, isNeedLogging] = useState(true);
+  const [userToken, setUserToken] = useState("")
+  const dispatch = useDispatch();
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    await firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(setToken(user.uid));
+        setUserToken(user.uid)
+      } else {
+        dispatch(setToken(""));
+      }
+    });
+    await checkToken();
+  };
+  const checkToken = async () => {
+    await isLoading(true);
+    if (userToken != null) {
+      isNeedLogging(false);
+    }
+  };
+  return (
+    <>
+      {loading ? (
+        <>
+          {needLogging ? (
+            <div className="login">
+              <div className="loginBox">
+                <LoginComponent></LoginComponent>
+              </div>
             </div>
-          </Button>
-        </Stack>
-      </div>
-    </div>
+          ) : (
+            <Home></Home>
+          )}
+        </>
+      ) : (
+        <LoadingComponent></LoadingComponent>
+      )}
+    </>
   );
 }
